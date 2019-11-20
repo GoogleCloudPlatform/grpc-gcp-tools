b5d6f5529d822b8f9db66b82331d6da9fffe177e
Qixuan Li <qixuanl@google.com>
2019-11-19 17:23:03 -0800

    add build script

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/GoogleCloudPlatform/grpc-gcp-go
+
+go 1.13
+
+require google.golang.org/grpc v1.25.1 // indirect
---
+++ release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ "$1" == "" ]; then
+  echo "USAGE:  $0 TOOL NAME" >&2
+  exit 1
+fi
+
+if [ ! -e $1 ]; then
+  echo $1": Tool not found." >&2
+  exit 1
+fi
+
+set -ex
+
+build_dp_check() {
+  cd dp_check && CGO_ENABLED=0 go build dp_check.go
+  file dp_check | grep 'statically linked'
+  if [ $? != 0 ]; then
+    echo "dp_check binary is not statically linked"
+  else
+    zip dp_check.zip dp_check
+  fi
+  rm -f dp_check
+}
+
+eval "build_$1"
